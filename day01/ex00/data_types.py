2734d5337a53d372a9617270e72cfdb7d6e2f582
Harras Rema <hrema@oa-c5.msk.21-school.ru>
2021-06-28 20:17:48 +0300

    day01: ex00 done

---
+++ day01/ex00/data_types.py
@@ -0,0 +1,25 @@
+def	typeof(var):
+	if (isinstance(var, bool)):
+		return ("bool")
+	if (isinstance(var, int)):
+		return ('int')
+	if (isinstance(var, str)):
+		return ('str')
+	if (isinstance(var, float)):
+		return ('float')
+	if (isinstance(var, list)):
+		return ('list')
+	if (isinstance(var, dict)):
+		return ('dict')
+	if (isinstance(var, tuple)):
+		return ('tuple')
+	if (isinstance(var, set)):
+		return ('set')
+
+def data_types():
+	var_list = [1, "Hello", 4.21, True, [1, 2, 3], {"name": "Alexander"}, (1, 4.21), {1, 2, 3}]
+	types_list = list(map(typeof, var_list))
+	print('[', ', '.join(types_list), ']', sep='')
+
+if __name__ == '__main__':
+	data_types()
