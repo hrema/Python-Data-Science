dee18f8a3b96c3cde5b14500a2d49a7061fd40cf
Harras Rema <hrema@oa-f5.msk.21-school.ru>
2021-07-01 15:59:30 +0300

    day02: ex00: edit first_class.py

--- day02/ex00/first_class.py
+++ day02/ex00/first_class.py
@@ -1,8 +1,6 @@
 class Must_read:
-	def	read_file(self, file_path):
-		with open(file_path) as f:
-			print(f.read())
+	with open("data.csv") as f:
+		print(f.read())
 
 if __name__ == "__main__":
-	mr = Must_read()
-	mr.read_file("data.csv")
+	Must_read()
