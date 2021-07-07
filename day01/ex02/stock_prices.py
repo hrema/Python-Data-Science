a4e6d166edde9a115a2321248ec9987a865f9113
Harras Rema <hrema@oa-f3.msk.21-school.ru>
2021-07-07 15:02:12 +0300

    day01: ex02: code refactor

--- day01/ex02/stock_prices.py
+++ day01/ex02/stock_prices.py
@@ -20,12 +20,12 @@ def	print_price_company(company):
 	COMPANIES_lower_key = {k.lower(): v for k, v in COMPANIES.items()}
 	company_lower = company.lower()
 	if company_lower not in COMPANIES_lower_key.keys():
-		print("Unknown company")
+		print('Unknown company')
 		return
 	else:
 		ticker = COMPANIES_lower_key[company_lower]
 		print(STOCKS[ticker])
 
-if __name__ == "__main__":
-	if (len(sys.argv) == 2):
+if __name__ == '__main__':
+	if len(sys.argv) == 2:
 		print_price_company(sys.argv[1])
