869b714a2f177be21ce68a6bb90eb95d6f353717
Harras Rema <hrema@oa-f3.msk.21-school.ru>
2021-07-07 15:09:11 +0300

    day01: ex08: code refactor

--- day01/ex08/letter_starter.py
+++ day01/ex08/letter_starter.py
@@ -1,11 +1,12 @@
 import sys
 
-if __name__ == "__main__":
-	if (len(sys.argv) != 2):
-		raise RuntimeError("Wrong number of arguments")
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		raise RuntimeError('Wrong number of arguments')
 	
 	email = sys.argv[1]
-	filename = "employees.tsv"
+	filename = 'employees.tsv'
 
 	with open(filename) as f:
 		lines = f.readlines()
@@ -13,14 +14,14 @@ if __name__ == "__main__":
 	lines_strip = [x.strip() for x in lines]
 	info_list = [x.split('\t') for x in lines_strip[1:]]
 	
-	desired_name = ""
+	desired_name = ''
 	for info in info_list:
-		if (info[2] == email):
+		if info[2] == email:
 			desired_name = info[0]
 			break
 	
-	if (desired_name == ""):
-		print("No such this e-mail")
+	if (desired_name == ''):
+		print('No such this e-mail')
 	else:
 		print(f'Dear {desired_name}, welcome to our team. '
 				'We are sure that it will be a pleasure to work with you. '
--- day01/ex08/names_extractor.py
+++ day01/ex08/names_extractor.py
@@ -1,8 +1,9 @@
 import sys
 
-if __name__ == "__main__":
-	if (len(sys.argv) != 2):
-		raise RuntimeError("Wrong number of arguments")
+
+if __name__ == '__main__':
+	if len(sys.argv) != 2:
+		raise RuntimeError('Wrong number of arguments')
 	
 	file_path = sys.argv[1]
 	with open(file_path) as f:
@@ -10,12 +11,12 @@ if __name__ == "__main__":
 	
 	emails_list = [x.strip() for x in emails_list]
 	
-	filename = "employees.tsv"
-	with open(filename, "w") as f:
-		f.write("Name\tSurname\tE-mail\n")
+	filename = 'employees.tsv'
+	with open(filename, 'w') as f:
+		f.write('Name\tSurname\tE-mail\n')
 		for email in emails_list:
 			email_split = email.split('@')
 			name_and_surname = email_split[0].split('.')
 			name_and_surname[0] = name_and_surname[0].title()
 			name_and_surname[1] = name_and_surname[1].title()
-			f.write(f"{name_and_surname[0]}\t{name_and_surname[1]}\t{email}\n")
+			f.write(f'{name_and_surname[0]}\t{name_and_surname[1]}\t{email}\n')
