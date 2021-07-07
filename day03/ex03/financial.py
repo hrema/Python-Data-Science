d16da10377c64d57bacfb3027ef0a06607263de9
Harras Rema <hrema@oa-f3.msk.21-school.ru>
2021-07-07 18:44:58 +0300

    day03: ex03: done

--- day03/ex03/financial.py
+++ day03/ex03/financial.py
@@ -6,35 +6,14 @@ import requests
 import os
 import re
 import json
+import time
 
-def	get_price(field, period, quarterl=0):
-	field_camel = field.split(" ")
-	field_camel = [x.replace(x[0], x[0].upper()) for x in field_camel]
-	if '&' in field_camel:
-		index = field_camel.index('&')
-		field_camel[index] = "And"
-	field_camel = ''.join(field_camel)
-	if (period == "trailing"):
-		price = quote_time_series_store.get("trailing" + field_camel)
-		if (price is None):
-			return ('-')
-		else:
-			if (len(price) > 0):
-				price = price[0]["reportedValue"]["raw"]
-			else:
-				return "-"
-	else:
-		price = quote_time_series_store.get("annual" + field_camel)
-		if price is None:
-			return "-"
-		else:
-			if (len(price) > 0):
-				price = price[quarterl]["reportedValue"]["raw"]
-			else:
-				return "-"
 
-	if "EPS" in field_camel:
-		return f"{price:.2f}"
+def price_formatting(price):
+	'''
+	Accepts a float or int price, formats it, and returns the price converted to string.
+	'''
+
 	sign = 1
 	if price < 0:
 		sign = -1
@@ -43,98 +22,100 @@ def	get_price(field, period, quarterl=0):
 	price = int(price)
 	price = list(str(price))
 	for i in range(-3, -(len(price) + 1), -4):
-		price.insert(i, ",")
+		price.insert(i, ',')
 	if sign == -1:
 		price.insert(0, '-')
 	price = ''.join(price)
 	return price
 
 
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		raise RuntimeError("Wrong numbre of arguments")
+def str_to_camel_style(string):
+	'''
+	Convert string to camel styles. "Hello World" -> "helloWorld"
+	'''
+
+	field_camel = string.split(' ')
+	field_camel = [x.replace(x[0], x[0].upper()) for x in field_camel]
+	field_camel = ''.join(field_camel)
+	return field_camel
+
+
+def	get_price(field, period, quarter=0):
+	'''
+	The function takes a table field, formats it camel style.
+	Finds the price of this field in a quarter, formats the price, and returns its string representation.
+	'''
+
+	if 'Available to' in field:
+		field = field.replace('Available to', 'Availto', 1)
+	if '&' in field:
+		field = field.replace('&', 'And')
+
+	field_camel = str_to_camel_style(field)
+
+	price = quote_time_series_store.get(period + field_camel)
+	if price is None:
+		return '-'
+	if period == 'trailing' and quarter != 0:
+		raise RuntimeError('Wrong quarter')
+
+	if len(price) > 0:
+		if price[quarter] is None:
+			return '-'
+		price = price[quarter]['reportedValue']['raw']
+	else:
+		return '-'
+
+	if 'EPS' in field_camel:
+		return f'{price:.2f}'
+	
+	price = price_formatting(price)
+	return price
+
+
+if __name__ == '__main__':
+	if len(sys.argv) != 3:
+		raise RuntimeError('Wrong numbre of arguments')
 	
 	ticker = sys.argv[1]
-	# field = sys.argv[2]
-	url = f"https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}"
+	field = sys.argv[2]
+	url = f'https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}'
 
 	try:
 		req = requests.get(url, headers={'User-Agent': 'Custom'})
-		if (req.status_code != 200):
-			raise RuntimeError(f"Server ruterned {req.status_code}")
-		if (req.url != url):
-			raise RuntimeError("Wrong ticker name")
+		if req.status_code != 200:
+			raise RuntimeError(f'Server ruterned {req.status_code}')
+		if req.url != url:
+			raise RuntimeError('Wrong ticker name')
 	except Exception:
 		raise
 
-	# with open(f"{ticker}.html", "w") as f:
+	# with open(f'{ticker}.html', 'w') as f:
 	# 	f.write(req.text)
 
-	soup = BeautifulSoup(req.text, "html.parser")
-	script = soup.find('script', text=re.compile("root.App.main"))
-	json_text = re.search(r"\s*root.App.main\s*=\s*({.*?})\s*;\s*$", str(script), flags=re.DOTALL | re.MULTILINE).group(1)
+	time.sleep(5)
+	soup = BeautifulSoup(req.text, 'html.parser')
+	script = soup.find('script', text=re.compile('root.App.main'))
+	json_text = re.search(r'\s*root.App.main\s*=\s*({.*?})\s*;\s*$', str(script), flags=re.DOTALL | re.MULTILINE).group(1)
 	data_dict = json.loads(json_text)
 
 	# data = json.dumps(data_dict, indent=4)
-	# with open(f"{ticker}.json", "w") as f:
+	# with open(f'{ticker}.json', 'w') as f:
 	# 	print(data, file=f)
 
-	quote_time_series_store = data_dict["context"]["dispatcher"]["stores"]["QuoteTimeSeriesStore"]["timeSeries"]
+	quote_time_series_store = data_dict['context']['dispatcher']['stores']['QuoteTimeSeriesStore']['timeSeries']
 
-	data = json.dumps(quote_time_series_store, indent=4)
-	with open(f"{ticker}_quote_time_series_store.json", "w") as f:
-		print(data, file=f)
+	# data = json.dumps(quote_time_series_store, indent=4)
+	# with open(f'{ticker}_quote_time_series_store.json', 'w') as f:
+	# 	print(data, file=f)
 
-	l = [
-		"Total Revenue",
-		"Operating Revenue",
-		"Cost of Revenue",
-		"Gross Profit",
-		"Operating Expense",
-		"Selling General and Administration",
-		"Research & Development",
-		"Operating Income",
-		"Net Non Operating Interest Income Expense",
-		"Interest Income Non Operating",
-		"Interest Expense Non Operating",
-		"Other Income Expense",
-		"Other Non Operating Income Expenses",
-		"Pretax Income",
-		"Tax Provision",
-		"Net Income Common Stockholders",
-		"Net Income",
-		"Net Income Including Noncontrolling Interests",
-		"Net Income Continuous Operations",
-		"Diluted NI Availto Com Stockholders",
-		"Basic EPS",
-		"Diluted EPS",
-		"Basic Average Shares",
-		"Diluted Average Shares",
-		"Total Operating Income as Reported",
-		"Total Expenses",
-		"Net Income from Continuing & Discontinued Operation",
-		"Normalized Income",
-		"Interest Income",
-		"Interest Expense",
-		"Net Interest Income",
-		"EBIT",
-		"EBITDA",
-		"Reconciled Cost of Revenue",
-		"Reconciled Depreciation",
-		"Net Income from Continuing Operation Net Minority Interest",
-		"Normalized EBITDA",
-		"Tax Rate for Calcs",
-		"Tax Effect of Unusual Items",
-	]
-
-	for field in l:
-		list_field = []
-		list_field.append(field)
-		list_field.append(get_price(field, "trailing"))
-		for i in range(3, -1, -1):
-			list_field.append(get_price(field, "annual", i))
-		print(tuple(list_field))
+	list_field = []
+	list_field.append(field)
+	list_field.append(get_price(field, 'trailing'))
+	for i in range(3, -1, -1):
+		list_field.append(get_price(field, 'annual', i))
+	print(tuple(list_field))
 	
-	# os.remove(f"{ticker}.html")
-	# os.remove(f"{ticker}.json")
-	# os.remove(f"{ticker}_quote_time_series_store.json")
+	# os.remove(f'{ticker}.html')
+	# os.remove(f'{ticker}.json')
+	# os.remove(f'{ticker}_quote_time_series_store.json')
