825352a03005598c354a09f0aedc0b2f2b1657e2
Harras Rema <hrema@oa-f3.msk.21-school.ru>
2021-07-15 12:31:04 +0300

    day04: ex00: done !

--- day04/README.md
+++ day04/README.md
@@ -1,6 +1,8 @@
 # Введение в Python: эффективные методы работы с кодом
 
-
+## ex00:
+Требуется написать скрипт, который будет сравнивать скорость работы создания списка через циклы и loop comprehension.
 
 # Использованные ресурсы
+
 https://webdevblog.ru/kogda-ispolzovat-list-comprehension-v-python/
--- day04/ex00/benchmark.py
+++ day04/ex00/benchmark.py
@@ -1,5 +1,7 @@
 #!/Users/hrema/.brew/bin/python3
 
+import timeit
+
 
 def loop(emails):
 	'''
@@ -18,9 +20,23 @@ def list_comprehension(emails):
 	The function creates a new list using list comprehension only gmail addresses and returns this list.
 	'''
 
-	gmail_emails = []
+	gmail_emails = [i for i in emails if 'gmail.com' in i]
+	return gmail_emails
 
 
 if __name__ == '__main__':
 	emails = ['john@gmail.com', 'james@gmail.com', 'alice@yahoo.com', 'anna@live.com', 'philipp@gmail.com'] * 5
-	print(loop(emails))
+	
+	loop_code = '''loop(emails)'''
+	lc_code = '''list_comprehension(emails)'''
+	count = 90_000_000
+	
+	loop_time = timeit.timeit(loop_code, number=count, globals=globals())
+	lc_time = timeit.timeit(lc_code, number=count, globals=globals())
+
+	if lc_time <= loop_time:
+		print('it is better to use a list comprehension')
+	else:
+		print('it is better to use a loop')
+	
+	print(min(lc_time, loop_time), 'vs', max(lc_time, loop_time))
