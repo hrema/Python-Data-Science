47b36df6105086f3e623d08fc14e47e4f9b725fd
Harras Rema <hrema@mi-h5.msk.21-school.ru>
2021-07-15 21:22:14 +0300

    day04: ex01: donegit add .

--- day04/README.md
+++ day04/README.md
@@ -3,6 +3,11 @@
 ## ex00:
 Требуется написать скрипт, который будет сравнивать скорость работы создания списка через циклы и loop comprehension.
 
+## ex01:
+Требуется в скрипт из ex00 добавить функцию, которая будет создавать лист с помощью функции map и сравнить скорость работы трех функций. 
+
 # Использованные ресурсы
 
 https://webdevblog.ru/kogda-ispolzovat-list-comprehension-v-python/
+
+https://stackoverflow.com/questions/5086430/how-to-pass-parameters-of-a-function-when-using-timeit-timer
---
+++ day04/ex01/benchmark.py
@@ -0,0 +1,73 @@
+#!/Users/hrema/.brew/bin/python3
+
+import timeit
+
+
+def loop(emails):
+	'''
+	The function creates a new list in a loop.
+	New list contain only gmail addresses.
+	Function returns new list.
+	'''
+	
+	gmail_emails = []
+	for i in emails:
+		if 'gmail.com' in i:
+			gmail_emails.append(i)
+	return gmail_emails
+
+
+def list_comprehension(emails):
+	'''
+	The function creates a new list using list comprehension.
+	New list contain only gmail addresses.
+	Function returns new list.
+	'''
+
+	gmail_emails = [i for i in emails if 'gmail.com' in i]
+	return gmail_emails
+
+
+def	func_map(emails):
+	'''
+	The function creates a new list using function map().
+	New list contain only gmail addresses.
+	Function returns new list.
+	'''
+
+	gmail_emails = map(lambda x: x if 'gmail.com' in x else None, emails)
+	return list(gmail_emails)
+
+
+if __name__ == '__main__':
+	emails = ['john@gmail.com', 'james@gmail.com', 'alice@yahoo.com', 'anna@live.com', 'philipp@gmail.com'] * 5
+	
+	loop_code = '''loop(emails)'''
+	lc_code = '''list_comprehension(emails)'''
+	lc_func_map = '''func_map(emails)'''
+	count = 90_000_000
+	
+	time = []
+	loop_time = timeit.timeit(loop_code, number=count, globals=globals())
+	time.append(loop_time)
+	lc_time = timeit.timeit(lc_code, number=count, globals=globals())
+	time.append(lc_time)
+	lc_func_map = timeit.timeit(lc_func_map, number=count, globals=globals())
+	time.append(lc_func_map)
+
+	sort_time = sorted(time)
+	min_time = sort_time[0]
+
+	if min_time == loop_time:
+		print('it is better to use a loop')
+	elif min_time == lc_time:
+		print('it is better to use a list comprehension')
+	elif min_time == lc_func_map:
+		print('it is better to use a map')
+
+	len_sort_time = len(sort_time)
+	for t in range(len_sort_time):
+		print(sort_time[t], end='')
+		if t + 1 != len_sort_time:
+			print(' vs ', end='')
+	print()
